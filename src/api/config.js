9f6c20752de1abcbe0034edae752d2e1df35ac12
Jhon Carreño <jhon7car@gmail.com>
2024-03-30 16:53:57 -0300

    fix

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const comicsApi = axios.create({
-    // baseURL: 'https://desafio-code-nodejs-jhon.onrender.com/api',
-    baseURL:'http://localhost:8080/api'
+    baseURL: 'https://desafio-code-nodejs-jhon.onrender.com/api',
+    // baseURL:'http://localhost:8080/api'
 });
 
 comicsApi.interceptors.request.use(config => {
